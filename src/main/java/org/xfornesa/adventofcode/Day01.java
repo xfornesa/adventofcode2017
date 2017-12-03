e66ed8c15ca241ea532cdf25c7523241f19fc77e
Xavier Fornes <xavier.fornes@scmspain.com>
2017-12-03 20:04:56 +0100

    day 01

---
+++ src/main/java/org/xfornesa/adventofcode/Day01.java
@@ -0,0 +1,18 @@
+package org.xfornesa.adventofcode;
+
+public class Day01 {
+
+  public int resolve(String digits) {
+    final String[] array = digits.split("");
+    int result = 0;
+    final int length = array.length;
+    for (int i = 0; i < length; i++) {
+      final String current = array[i];
+      final String following = array[(i+1) % length];
+      if (current.equals(following)) {
+        result += Integer.valueOf(current);
+      }
+    }
+    return result;
+  }
+}
