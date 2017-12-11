cdd7bce367cfe7bd079447c833d53db8e5fc084a
Xavier Fornes <xavier.fornes@scmspain.com>
2017-12-11 22:09:33 +0100

    day 11

---
+++ src/main/java/org/xfornesa/adventofcode/Day11.java
@@ -0,0 +1,45 @@
+package org.xfornesa.adventofcode;
+
+public class Day11 {
+  public int hexGrid(String input) {
+
+    int x = 0;
+    int y = 0;
+    int z = 0;
+    for(String direction: input.split(",")) {
+      switch (direction) {
+        case "n":
+          //x;
+          y++;
+          z--;
+          break;
+        case "s":
+          //x;
+          y--;
+          z++;
+          break;
+        case "ne":
+          x++;
+          //y;
+          z--;
+          break;
+        case "sw":
+          x--;
+          //y;
+          z++;
+          break;
+        case "nw":
+          x--;
+          y++;
+          //z;
+          break;
+        case "se":
+          x++;
+          y--;
+          //z;
+          break;
+      }
+    }
+    return Math.max(Math.abs(x), Math.max(Math.abs(y), Math.abs(z)));
+  }
+}
---
+++ src/test/java/org/xfornesa/adventofcode/Day11Test.java
@@ -0,0 +1,24 @@
+package org.xfornesa.adventofcode;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.InputStream;
+
+public class Day11Test {
+  @Test
+  public void tests() throws Exception {
+    final Day11 sut = new Day11();
+
+    Assert.assertEquals(3, sut.hexGrid("ne,ne,ne"));
+    Assert.assertEquals(0, sut.hexGrid("ne,ne,sw,sw"));
+    Assert.assertEquals(2, sut.hexGrid("ne,ne,s,s"));
+    Assert.assertEquals(3, sut.hexGrid("se,sw,se,sw,sw"));
+
+    final InputStream resourceAsStream = this.getClass().getResourceAsStream("/input11.txt");
+    String input = IOUtils.toString(resourceAsStream, "UTF-8");
+    Assert.assertEquals(722, sut.hexGrid(input));
+
+  }
+}
