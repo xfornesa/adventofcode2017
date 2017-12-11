ea092de309bbae34039d49329cc877f434dd5c2e
Xavier Fornes <xavier.fornes@scmspain.com>
2017-12-11 22:31:35 +0100

    day 01 - part 2

--- src/main/java/org/xfornesa/adventofcode/Day01.java
+++ src/main/java/org/xfornesa/adventofcode/Day01.java
@@ -15,4 +15,18 @@ public class Day01 {
     }
     return result;
   }
+
+  public int halfAround(String digits) {
+    final String[] array = digits.split("");
+    int result = 0;
+    final int length = array.length;
+    for (int i = 0; i < length; i++) {
+      final String current = array[i];
+      final String following = array[(i+array.length/2) % length];
+      if (current.equals(following)) {
+        result += Integer.valueOf(current);
+      }
+    }
+    return result;
+  }
 }
