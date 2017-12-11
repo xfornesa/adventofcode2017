49e6e8435883001c7ef7d1572d29b13ac8eb1d5d
Xavier Fornes <xavier.fornes@scmspain.com>
2017-12-11 23:15:16 +0100

    day 04 - part 2

--- src/main/java/org/xfornesa/adventofcode/Day04.java
+++ src/main/java/org/xfornesa/adventofcode/Day04.java
@@ -2,6 +2,8 @@ package org.xfornesa.adventofcode;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day04 {
 
@@ -19,5 +21,26 @@ public class Day04 {
       }
     }
     return result;
+  }
+
+  public int isPassPhraseValidWithoutAnagrams(String input) {
+    int result = 0;
+    for (String row: input.split("\n")) {
+      final Set<String> words = new HashSet<>();
+      boolean valid = true;
+      for (String value: row.split(" ")) {
+        final String valueReordered = reOrder(value);
+        valid = valid && (!words.contains(valueReordered));
+        words.add(valueReordered);
+      }
+      if (valid) {
+        result++;
+      }
+    }
+    return result;
+  }
+
+  private String reOrder(String value) {
+    return Stream.of(value.split("")).sorted().collect(Collectors.joining());
   }
 }
