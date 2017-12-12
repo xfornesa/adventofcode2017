7016278fb5274cf7fcf65731c18e3e53e7b412a8
Xavier Fornes <xavier.fornes@scmspain.com>
2017-12-12 22:12:20 +0100

    day 09 - part 2

--- src/main/java/org/xfornesa/adventofcode/Day09.java
+++ src/main/java/org/xfornesa/adventofcode/Day09.java
@@ -38,4 +38,36 @@ public class Day09 {
     }
     return result;
   }
+
+  public int garbageChars(String input) {
+    final String[] chars = input.split("");
+    int i = 0;
+    int result = 0;
+    boolean garbage = false;
+    while (i<chars.length) {
+      final String currentChar = chars[i];
+      switch (currentChar) {
+        case "<":
+          if (garbage) {
+            result++;
+          }
+          garbage = true;
+          break;
+        case ">":
+          garbage = false;
+          break;
+        case "!":
+          i++; // ignore following char
+          break;
+        default:
+          if (garbage) {
+            result++;
+          }
+          // Do nothing at all
+          break;
+      }
+      i++;
+    }
+    return result;
+  }
 }
--- src/test/java/org/xfornesa/adventofcode/Day09Test.java
+++ src/test/java/org/xfornesa/adventofcode/Day09Test.java
@@ -21,9 +21,18 @@ public class Day09Test {
     Assert.assertEquals(9, sut.groupScoring("{{<!!>},{<!!>},{<!!>},{<!!>}}"));
     Assert.assertEquals(3, sut.groupScoring("{{<a!>},{<a!>},{<a!>},{<ab>}}"));
 
+    Assert.assertEquals(0, sut.garbageChars("<>"));
+    Assert.assertEquals(17, sut.garbageChars("<random characters>"));
+    Assert.assertEquals(3, sut.garbageChars("<<<<>"));
+    Assert.assertEquals(2, sut.garbageChars("<{!>}>"));
+    Assert.assertEquals(0, sut.garbageChars("<!!>"));
+    Assert.assertEquals(0, sut.garbageChars("<!!!>>"));
+    Assert.assertEquals(10, sut.garbageChars("<{o\"i!a,<{i<a>"));
+
 
     final InputStream resourceAsStream = this.getClass().getResourceAsStream("/input09.txt");
     String input = IOUtils.toString(resourceAsStream, "UTF-8");
     Assert.assertEquals(11846, sut.groupScoring(input));
+    Assert.assertEquals(6285, sut.garbageChars(input));
   }
 }
